bf7aa1bb4525b30ed74a6a9c77a0290e0269b839
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-05-22 21:03:31 +0200

    problem

---
+++ server.js
@@ -0,0 +1,20 @@
+﻿var fs = require('fs');
+var app = express();
+var stringifyFile = req.params.note;
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function(req, res) {
+	fs.readFile('./test.json', 'utf8', function(err, data) {
+		if (err) throw err;
+		stringifyFile = data
+		res.send(data);
+	});
+});
+app.post('/updateNote/:note', function(req, res) {
+	fs.writeFile('./test.json', stringifyFile, function(err) {
+    If (err) throw err;
+    console.log('file updated');
+    });
+});
+app.listen(3000);
